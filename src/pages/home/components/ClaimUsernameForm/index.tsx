88247fcf5157fff698a0843a58e8bc832b9c6828
gabriellafaria <gabisouf@gmail.com>
2023-09-06 11:28:46 -0300

    feat: :sparkles: add ClaimUsernameForm component
    Component for the form in the home page.

---
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -0,0 +1,20 @@
+import { Button, TextInput } from '@ignite-ui/react'
+import { Form } from './styles'
+import { ArrowRight } from 'phosphor-react'
+
+export function ClaimUsernameForm() {
+  return (
+    <Form as="form">
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuário"
+        crossOrigin={undefined}
+      />
+      <Button size="sm" type="submit">
+        Reservar
+        <ArrowRight />
+      </Button>
+    </Form>
+  )
+}
