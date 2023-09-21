e28373900b69e167d96208d3a17a5d30401e556b
gabriellafaria <gabisouf@gmail.com>
2023-09-21 17:01:20 -0300

    chore: init calandar logic

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -8,21 +8,62 @@ import {
   CalandarTitle,
 } from './styles'
 import { getWeekDays } from '@/utils/get-week-days'
+import { useMemo, useState } from 'react'
+import dayjs from 'dayjs'
 
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set('date', 1)
+  })
+
+  function handlePreviousMount() {
+    const previousMounthDate = currentDate.subtract(1, 'month')
+    setCurrentDate(previousMounthDate)
+  }
+
+  function handleNextMonth() {
+    const nextMounthDate = currentDate.add(1, 'month')
+    setCurrentDate(nextMounthDate)
+  }
+
   const shortWeekDays = getWeekDays({ short: true })
+
+  const currentMonth = currentDate.format('MMMM')
+  const currentYear = currentDate.format('YYYY')
+
+  const calendarWeeks = useMemo(() => {
+    const daysIMountArray = Array.from({
+      length: currentDate.daysInMonth(),
+    }).map((_, i) => {
+      return currentDate.set('date', i + 1)
+    })
+
+    const firstWeekDay = currentDate.get('day')
+
+    const previousMonthFillArray = Array.from({
+      length: firstWeekDay,
+    })
+      .map((_, i) => {
+        return currentDate.subtract(i + 1, 'day')
+      })
+      .reverse()
+
+    return [...previousMonthFillArray, ...daysIMountArray]
+  }, [currentDate])
+
+  console.log(calendarWeeks)
 
   return (
     <CalandarContainer>
       <CalandarHeader>
         <CalandarTitle>
-          Dezembro <span>2023</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalandarTitle>
         <CalandarActions>
-          <button>
+          <button onClick={handlePreviousMount} title="Previous Mount">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title="Next Mount">
             <CaretRight />
           </button>
         </CalandarActions>
